87600b6c478f1c77a26e8d314ff1a9e8533b0c15
Victor Longon <victorlongon@SE-C02R865AG8WP.local>
2017-05-02 09:41:37 +0200

    add TimeUp component

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,147 +4,160 @@ import HolesList from './HolesList';
 import Logo from './Logo';
 import TopBar from './TopBar';
 import Counter from './Counter';
+import TimeUp from './TimeUp';
 import * as utils from '../utils';
 import './Game.css';
 
 
 class Game extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hasStarted: false,
-      score: 0,
-      // TODO: get those values from a form instead
-      duration: 10, // seconds
-      // initally set to the same as duration
-      remainingTime: 10,
-      minSpeed: 200,
-      maxSpeed: 1000,
-      holes: utils.generateHoles({ amount: 6 })
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			hasStarted: false,
+			hasEnded: false,
+			score: 0,
+			// TODO: get those values from a form instead
+			duration: 10, // seconds
+			// initally set to the same as duration
+			remainingTime: 10,
+			minSpeed: 200,
+			maxSpeed: 1000,
+			holes: utils.generateHoles({ amount: 6 })
+		}
 
-    // flags used for showing random holes logic, but we don't want those
-    // as state since we no re-render is necessary when these change
-    this.lastHole = null;
-    this.timeUp = false;
-    this.timeout = null;
-    this.remainingInterval = null;
-  }
+		// flags used for showing random holes logic, but we don't want those
+		// as state since we no re-render is necessary when these change
+		this.lastHole = null;
+		this.timeUp = false;
+		this.timeout = null;
+		this.remainingInterval = null;
+	}
 
-  getRandomHole = () => {
-    const { holes } = this.state;
-    const currentHole = holes[utils.generateRandomIndex(holes)];
+	getRandomHole = () => {
+		const { holes } = this.state;
+		const currentHole = holes[utils.generateRandomIndex(holes)];
 
-    if (currentHole === this.lastHole) {
-      return this.getRandomHole(holes);
-    }
-    this.lastHole = currentHole;
-    return currentHole;
-  }
+		if (currentHole === this.lastHole) {
+			return this.getRandomHole(holes);
+		}
+		this.lastHole = currentHole;
+		return currentHole;
+	}
 
-  showMole = () => {
-    const { minSpeed, maxSpeed } = this.state;
-    const time = utils.generateRandomTime(minSpeed, maxSpeed);
-    const { id: randomId } = this.getRandomHole();
+	showMole = () => {
+		const { minSpeed, maxSpeed } = this.state;
+		const time = utils.generateRandomTime(minSpeed, maxSpeed);
+		const { id: randomId } = this.getRandomHole();
 
-    this.setState(prevState => ({
-      holes: prevState.holes.map(hole =>
-        hole.id === randomId ? { ...hole, isActive: true } : hole
-      )
-    }));
+		this.setState(prevState => ({
+			holes: prevState.holes.map(hole =>
+				hole.id === randomId ? { ...hole, isActive: true } : hole
+			)
+		}));
 
-    setTimeout(() => {
-      this.setState(prevState => ({
-        holes: prevState.holes.map(hole =>
-          hole.id === randomId ? { ...hole, isActive: false } : hole
-        )
-      }));
+		setTimeout(() => {
+			this.setState(prevState => ({
+				holes: prevState.holes.map(hole =>
+					hole.id === randomId ? { ...hole, isActive: false } : hole
+				)
+			}));
 
-      !this.timeUp && this.showMole();
-    }, time);
+			!this.timeUp && this.showMole();
+		}, time);
 
-  }
+	}
 
-  onMoleClick = id => {
-    this.setState(prevState => ({
-      holes: prevState.holes.map(hole =>
-        hole.id === id ? { ...hole, isActive: false } : hole
-      ),
-      score: prevState.score + 1
-    }));
-  }
+	onMoleClick = id => {
+		this.setState(prevState => ({
+			holes: prevState.holes.map(hole =>
+				hole.id === id ? { ...hole, isActive: false } : hole
+			),
+			score: prevState.score + 1
+		}));
+	}
 
-  start = e => {
-    this.remainingTime = this.state.duration;
-    this.reset();
-    this.timeUp = false;
-    this.showMole();
-    this.timeout = setTimeout(() =>
-      this.onEnd(), this.state.duration * 1000
-    );
-    this.setState({ hasStarted: true });
-    this.remainingInterval = setInterval(() => {
-      this.setState({ remainingTime: this.state.remainingTime - 1 });
-      (this.state.remainingTime === 0) && clearInterval(this.remainingInterval);
-    }, 1000)
-  }
+	start = e => {
+		this.remainingTime = this.state.duration;
+		this.reset();
+		this.timeUp = false;
+		this.showMole();
+		this.timeout = setTimeout(() =>
+			this.onEnd(), this.state.duration * 1000
+		);
+		this.setState({ hasStarted: true });
+		this.remainingInterval = setInterval(() => {
+			this.setState({ remainingTime: this.state.remainingTime - 1 });
+			(this.state.remainingTime === 0) && clearInterval(this.remainingInterval);
+		}, 1000)
+	}
 
-  reset = e => {
-    this.setState(prevState => ({
-      holes: prevState.holes.map(
-        hole => ({ ...hole, isActive: false })
-      ),
-      score: 0,
-      hasStarted: false,
-      remainingTime: this.state.duration
-    }));
-    this.timeUp = true;
-    clearInterval(this.remainingInterval);
-    clearTimeout(this.timeout);
-  }
+	reset = e => {
+		this.setState(prevState => ({
+			holes: prevState.holes.map(
+				hole => ({ ...hole, isActive: false })
+			),
+			score: 0,
+			hasStarted: false,
+			hasEnded: false,
+			remainingTime: this.state.duration
+		}));
+		this.timeUp = true;
+		clearInterval(this.remainingInterval);
+		clearTimeout(this.timeout);
+	}
 
-  onEnd = () => {
-    this.timeUp = true;
-    this.setState({ hasStarted: false });
-    // TODO show a time's up funny text
-  }
+	onEnd = () => {
+		this.timeUp = true;
+		this.setState({ 
+			hasStarted: false, 
+			hasEnded: true 
+		});
+		// TODO show a time's up funny text
+	}
 
-  render() {
-    const {
-      state: {
-        score,
-      holes,
-      hasStarted,
-      remainingTime
-      },
-      start,
-      reset,
-      onMoleClick
-   } = this;
+	render() {
+		const {
+    	state: {
+			score,
+				holes,
+				hasStarted,
+				hasEnded,
+				remainingTime
+			},
+				start,
+				reset,
+				onMoleClick
+			} = this;
 
-    return (
-      <div className="game">
-        <TopBar
-          onStart={start}
-          onReset={reset}
-        />
-        <Logo height={100} />
-        <ScoreBoard 
-          title="(Re)Whack-a-mole!" 
-          score={score} 
-        />
-        <Counter
-          className="game__counter"
-          isVisible={hasStarted}
-          time={remainingTime}
-        />
-        <HolesList 
-          items={holes} 
-          onMoleClick={onMoleClick} 
-        />
-      </div>
-    );
-  }
+		return (
+			<div className="game">
+				<TopBar
+					onStart={start}
+					onReset={reset}
+				/>
+				<Logo height={100} />
+				<ScoreBoard
+					title="(Re)Whack-a-mole!"
+					score={score}
+				/>
+				<Counter
+					className="game__counter"
+					isVisible={hasStarted}
+					time={remainingTime}
+				/>
+				<HolesList
+					items={holes}
+					onMoleClick={onMoleClick}
+				/>
+				<TimeUp 
+					show={hasEnded} 
+					text="Time's up!" 
+					tag="h2"
+					onClick={() => reset()} 
+				/>
+			</div>
+		);
+	}
 }
 
 export default Game;
